9c1a2a721f8c27aa6a0d25141505923298568aae
Fadhel <fadhel.s.hashem@gmail.com>
2026-07-09 12:04:32 +0300

    connect to mongoose

--- server.js
+++ server.js
@@ -1,14 +1,27 @@
+const dotenv = require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
+const mongoose = require('mongoose')
+const dns = require('node:dns')
+dns.setServers(['8.8.8.8', '1.1.1.1'])
 
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () =>{
+    console.log(`Connected to MongoDB ${mongoose.connection.name}`)
+})
+
 app.use(morgan('dev'))
 
 
-app.get('/' , async (req, res) =>{
-res.render('home.ejs')
+
+
+app.get('/', async (req, res) => {
+    res.render('home.ejs')
 })
 
-app.listen(3000, ()=>{
-    console.log("Listning on port 3000")
+
+app.listen(3000, function(){
+    console.log('Listening on port 3000')
 })
